09408cab4c366eb99ad3c54b97dcca061d83094b
Jitesh Patil <jitesh@yapapaya.com>
2017-03-27 23:23:09 +0530

    Adds a gulp task for concatenating & uglifying javascripts

--- functions.php
+++ functions.php
@@ -75,7 +75,7 @@ function adi_enqueue_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-	wp_enqueue_script( 'adi-script', get_template_directory_uri() . '/assets/js/functions.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'adi-script', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), null, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'adi_enqueue_scripts' );
--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,40 @@
  *
  * @since 1.0.0
  */
-var gulp = require( 'gulp' );
+var gulp 	= require( 'gulp' ),
+	del 	= require( 'del' ),
+	concat 	= require( 'gulp-concat' ),
+	uglify 	= require( 'gulp-uglify' );
 
 /**
  * Defines the default gulp task.
  *
  * @since 1.0.0
  */
-gulp.task( 'default', function() {
+gulp.task( 'default', [ 'scripts' ], function() {
 
+} );
+
+/**
+ * Defines the scripts task.
+ *
+ * @since 1.0.0
+ */
+gulp.task( 'scripts', [ 'cleanup' ], function() {
+	return gulp.src( 'assets/js/**/*.js' )
+		.pipe( concat( 'scripts.min.js' ) )
+		.pipe( gulp.dest( 'assets/js' ) )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'assets/js' ) );
+} );
+
+/**
+ * Defines the cleanup task.
+ *
+ * @since 1.0.0
+ */
+gulp.task( 'cleanup', function() {
+	return del( [
+		'assets/js/scripts.min.js',
+	] );
 } );
