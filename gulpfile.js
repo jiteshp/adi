98dab25642701731cd1b8a58dc66e15f1cf987e9
Jitesh Patil <jitesh@yapapaya.com>
2017-03-27 23:00:22 +0530

    Adds the default Gulp task

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * Theme build
+ *
+ * Defines the Gulp tasks used to build the theme.
+ *
+ * @since 1.0.0
+ */
+var gulp = require( 'gulp' );
+
+/**
+ * Defines the default gulp task.
+ *
+ * @since 1.0.0
+ */
+gulp.task( 'default', function() {
+
+} );
